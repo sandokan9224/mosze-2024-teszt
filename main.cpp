b13482cb19e2a4fec09dfac4c1bb9cbb7bc2cd74
bucsubarni <bucsubarni@gmail.com>
2024-09-17 20:11:25 +0200

    Hibák jelezve és javítva

--- main.cpp
+++ main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 
-constexpr int N_ELEMENTS = 100;
+const int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];            //szintaktikai hiba, rossz konstans névhasználat
-    std::cout << '1-100 ertekek duplazasa'      //; hiánya, "" használata kell '' helyett
-    for (int i = 0;)                        //értelmetlen for ciklus
+    int *b = new int[N_ELEMENTS];            //szintaktikai hiba, rossz konstans névhasználat .... javítva
+    std::cout << "1-100 ertekek duplazasa";      //; hiánya, "" használata kell '' helyett ... javítva
+    for (int i = 0; i<N_ELEMENTS; i++)                        //értelmetlen for ciklus ... javítva
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)                 //helytelen for ciklus
+    for (int i = 0; i<N_ELEMENTS; i++)                 //helytelen for ciklus... javítva
     {
-        std::cout << "Ertek:"                  // ; hiány ,illetve kimenet hiányzik
+        std::cout << "Ertek:"  << b[i] << std::endl;                 // ; hiány ,illetve kimenet hiányzik ... javítva
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                                  // inicializálás hiánya
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;                                  // inicializálás hiánya ... javítva
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]                       //; hiány
+        atlag += b[i];                       //; hiány ... javítva
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+
+    delete [] b;
     return 0;
 }
 
-// mermória felszabadítás
+// mermória felszabadítás ... javítva
