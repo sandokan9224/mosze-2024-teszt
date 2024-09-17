db3d0fa62107b94a4afeeee8dc64fa7d45bc2ac4
bucsubarni <bucsubarni@gmail.com>
2024-09-17 16:13:29 +0200

    Javított kód

--- main.cpp
+++ main.cpp
@@ -20,22 +20,6 @@ int main()
     {
         atlag += b[i];
     }
-    int *b = new int[N_ELEMENTS];
-    std::cout << "1-100 ertekek duplazasa";
-    for (int i = 0; i<N_ELEMENTS; i++)
-    {
-        b[i] = i * 2;
-    }
-    for (int i = 0; i<N_ELEMENTS; i++)
-    {
-        std::cout << "Ertek:" << b[i] << std::endl;
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag = 0;
-    for (int i = 0; i < N_ELEMENTS; i++)
-    {
-        atlag += b[i];
-    }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     
