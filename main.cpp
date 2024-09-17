bda0431641812399765592ac4c75b0c28f97dd44
bucsubarni <bucsubarni@gmail.com>
2024-09-17 14:59:42 +0200

    A hibák kommentben jelezve lettek

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS];            //szintaktikai hiba, rossz konstans névhasználat
+    std::cout << '1-100 ertekek duplazasa'      //; hiánya, "" használata kell '' helyett
+    for (int i = 0;)                        //értelmetlen for ciklus
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)                 //helytelen for ciklus
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"                  // ; hiány ,illetve kimenet hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
+    int atlag;                                  // inicializálás hiánya
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i]                       //; hiány
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
+// mermória felszabadítás
